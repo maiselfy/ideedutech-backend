ed0aae7ce7650e069db225a3cad13dee5db61107
Ítalo Lima <italolima534@gmail.com>
2022-02-18 09:49:40 -0300

    feat: inital version of database modeling

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ServiceModule } from './auth/service/service.module';
+import { AuthModule } from './service/auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, ServiceModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
