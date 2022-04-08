0341b739d7b277668f959f4bd4cbb8484a422227
Lucas do Nascimento Diniz <lucaszf.ln@gmail.com>
2022-04-07 21:41:19 -0300

    feat: add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { setupSwagger } from './swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setupSwagger(app);
   await app.listen(3000);
 }
 bootstrap();
