a0212a2486eef6b475b06b1feca07c6a9bff1ace
Marcos Gênesis da Silva <marcosgenesisof@gmail.com>
2022-04-22 14:54:14 -0300

    chore: school students listing

--- src/modules/student/controller/student.controller.ts
+++ src/modules/student/controller/student.controller.ts
@@ -9,6 +9,7 @@ import {
   Delete,
 } from '@nestjs/common';
 import ListEntitiesForSchoolDTO from '../dtos/listEntitiesForSchool.dto';
+import { User } from 'src/modules/user/decorators/user.decorator';
 
 @Controller('student')
 export class StudentController {
@@ -20,10 +21,8 @@ export class StudentController {
   @Get()
   findAll() {}
 
-  @Get('/list')
-  findStudentsBySchool(
-    @Body() { schoolId, managerId }: ListEntitiesForSchoolDTO,
-  ) {
-    return this.studentService.findBySchool({ schoolId, managerId });
+  @Get('/school/:schoolId')
+  findStudentsBySchool(@User() user, @Param('schoolId') schoolId: string) {
+    return this.studentService.findBySchool({ schoolId, managerId: user.id });
   }
 }
