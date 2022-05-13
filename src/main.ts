2e3b40bfad86ff6bc61dd05455c0ba4a8ef607ee
Ítalo Lima <italolima534@gmail.com>
2022-05-13 10:15:06 -0300

    chore: enable cors for all origins

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,17 @@ async function bootstrap() {
 
   setupSwagger(app);
 
-  app.enableCors();
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
