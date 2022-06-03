2cdbd30c9209d3be7163efb6c2d35945e2630c7c
Fabricio N Pinheiro <fabricionpinheiro7@gmail.com>
2022-06-03 08:47:11 -0300

    docs:update swagger.

--- src/modules/evaluation/dtos/createEvaluation.dto.ts
+++ src/modules/evaluation/dtos/createEvaluation.dto.ts
@@ -1,14 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmpty, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export default class CreateEvaluationDTO {
+  @ApiProperty({
+    example: '0ac5b816-3716-4b80-b090-37bed6ba37b2',
+  })
   @IsString()
   @IsNotEmpty()
   testId: string;
 
+  @ApiProperty({
+    example: '8bc5b816-3716-4b80-b090-37bed6ba37b2',
+  })
   @IsString()
   @IsNotEmpty()
   studentId: string;
 
+  @ApiProperty({
+    example: '10',
+  })
   @IsOptional()
   rate: number;
 
