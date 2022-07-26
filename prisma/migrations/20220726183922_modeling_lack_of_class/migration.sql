972c36680deb4a7c9f768a2230216973cad79634
Ítalo Lima <italolima534@gmail.com>
2022-07-26 15:39:41 -0300

    chore: fixing problems with schema

---
+++ prisma/migrations/20220726183922_modeling_lack_of_class/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `date` on the `LackOfClass` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "LackOfClass" DROP COLUMN "date";
