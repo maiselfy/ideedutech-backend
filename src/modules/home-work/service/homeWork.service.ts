f485e981cfe65d70a51270e6587bf2d73ab6fd9f
Ítalo Lima <italolima534@gmail.com>
2022-08-23 21:59:28 -0300

    fix: adding attachment on return of detail of homework

--- src/modules/home-work/service/homeWork.service.ts
+++ src/modules/home-work/service/homeWork.service.ts
@@ -570,6 +570,7 @@ export class HomeWorkService {
         dueDate: true,
         description: true,
         name: true,
+        attachement: true,
       },
     });
 
@@ -606,6 +607,10 @@ export class HomeWorkService {
       disciplineName: homeWork.discipline.name,
       className: homeWork.discipline.class.name,
       students: formattedStudents,
+      attachment: {
+        name: homeWork.attachement.split('_')[1],
+        url: homeWork.attachement,
+      },
       qtdStudents: homeWork.discipline.class.students.length,
       //submissions: formattedEvaluativeDelivery,
       // pendingSubmissions:
