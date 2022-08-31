fc872d76bde4caa26b83c036fd0ec9d6412413e4
Ítalo Lima <italolima534@gmail.com>
2022-08-31 09:02:19 -0300

    hotfix: adding properties on return of homework detail

--- src/modules/home-work/service/homeWork.service.ts
+++ src/modules/home-work/service/homeWork.service.ts
@@ -556,6 +556,7 @@ export class HomeWorkService {
                         id: true,
                         rate: true,
                         studentId: true,
+                        attachement: true,
                       },
                       orderBy: {
                         createdAt: 'desc',
@@ -575,6 +576,8 @@ export class HomeWorkService {
         description: true,
         name: true,
         attachement: true,
+        createdAt: true,
+        updatedAt: true,
       },
     });
 
@@ -595,6 +598,7 @@ export class HomeWorkService {
           avatar: student.user.avatar,
           submission: {
             id: submissionOfStudent?.id,
+            archive: submissionOfStudent?.attachement,
             rate: submissionOfStudent?.rate,
           },
         };
@@ -616,6 +620,8 @@ export class HomeWorkService {
         url: homeWork.attachement,
       },
       qtdStudents: homeWork.discipline.class.students.length,
+      createdAt: homeWork.createdAt,
+      updatedAt: homeWork.updatedAt,
       //submissions: formattedEvaluativeDelivery,
       // pendingSubmissions:
       //   homeWork.discipline.class.students.length -
