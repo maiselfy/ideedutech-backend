fccade602dec87da52b9827f7fd5a6e816cff831
Ítalo Lima <italolima534@gmail.com>
2022-11-22 22:15:14 -0300

    chore: adding swagger in new dto: update student

--- src/modules/student/dtos/updateStudent.dto.ts
+++ src/modules/student/dtos/updateStudent.dto.ts
@@ -1,23 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Gender } from '@prisma/client';
 import { IsEmail, IsEmpty, IsOptional, IsString } from 'class-validator';
 
 export default class UpdateStudentDTO {
+  @ApiProperty()
   @IsString()
   @IsOptional()
   name?: string;
 
+  @ApiProperty()
   @IsEmail()
   @IsOptional()
   email?: string;
 
+  @ApiProperty()
   @IsString()
   @IsOptional()
   birthDate?: Date;
 
+  @ApiProperty()
   @IsString()
   @IsOptional()
   phone?: string;
 
+  @ApiProperty()
   @IsString()
   @IsOptional()
   gender?: Gender;
