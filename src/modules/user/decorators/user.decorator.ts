0fdfa0b4c4fdad0cdd45d1645019247ba1b11b01
Ítalo Lima <italolima534@gmail.com>
2023-01-23 23:04:16 -0300

    hotfix: fixing master bug

--- src/modules/user/decorators/user.decorator.ts
+++ src/modules/user/decorators/user.decorator.ts
@@ -1,16 +1,9 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-import { findSchoolByUser } from 'src/shared/utils/findSchoolByUser.util';
-
 export const User = createParamDecorator(
   async (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
 
-    const school = await findSchoolByUser(request.user.id);
-
-    return {
-      ...request.user,
-      school: school ? school.name : undefined,
-    };
+    return request.user;
   },
 );
